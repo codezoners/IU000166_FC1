1141d495ce6ad0dde1bb73f9beca3f053e33e578
Nick (Git client) <nick@cassiel.com>
2023-11-09 16:33:50 +0000

    Scratch.

--- S07/scratch.py
+++ S07/scratch.py
@@ -12,19 +12,25 @@ import shapes
 import importlib
 importlib.reload(shapes)
 
-t.reset()
-t.up()
-t.speed(0)
-# By default, we want the pen to be up.
+def clear():
+    t.reset()
+    t.up()
+    t.speed(0)
+    # By default, we want the pen to be up.
 
-t.backward(300)
-t.right(90)
-t.backward(300)
-t.left(90)
+    t.backward(300)
+    t.right(90)
+    t.backward(300)
+    t.left(90)
+
+clear()
 
 for y in range(10):
     for x in range(10):
-        shapes.square(t, 50)
+
+        c = "blue" if x < 3 else "white" if x < 7 else "red"
+
+        shapes.square(t, 50, True, c)
         t.forward(60)
 
     t.backward(600)
