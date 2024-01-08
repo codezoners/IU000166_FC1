0e5ade73c1824ab5ebdb43895dd5fb9fd4f90870
Nick (Git client) <nick@cassiel.com>
2024-01-08 15:52:57 +0000

    Trees.

--- Assignment/tree.py
+++ Assignment/tree.py
@@ -1,5 +1,6 @@
 import turtle
 import os
+import random
 
 t = turtle.Turtle()
 
@@ -12,19 +13,26 @@ def save_to(stem):
     t.getscreen().getcanvas().postscript(file=psfile)
     os.system(" ".join(["convert", psfile, jpgfile]))
 
-def tree(depth, dist):
+def randomise(rand_pc):
+    if rand_pc > 0:
+        return random.randrange(100 - rand_pc, 100 + rand_pc) / 100
+    else:
+        return 1
+
+def tree(depth, dist, angle=20, scale=0.6, rand_pc=0):
     # We assume we're at the base of the trunk, pointing up:
     t.forward(dist)
 
     if depth > 0:
-        t.left(20)
-        tree(depth - 1, dist / 1.5)
-        t.right(40)
-        tree(depth - 1, dist / 1.5)
-        t.left(20)
+        t.left(angle * randomise(rand_pc))
+        tree(depth - 1, dist * scale * randomise(rand_pc))
+        t.right(angle * 2 * randomise(rand_pc))
+        tree(depth - 1, dist * scale * randomise(rand_pc))
+        t.left(angle * randomise(rand_pc))
 
     t.backward(dist)
 
+# Simple example in docs:
 t.reset()
 t.screen.setup(400, 400)
 teleport(0, -150)
@@ -34,3 +42,28 @@ t.speed("fastest")
 tree(1, 100)
 t.hideturtle()
 save_to("tree")
+
+# More like a forest:
+t.reset()
+t.screen.setup(800, 400)
+
+teleport(-200, -150)
+t.width(3)
+t.left(90)
+t.speed("fastest")
+tree(3, 100)
+save_to("tree_3")
+
+teleport(0, -150)
+t.width(2)
+t.speed("fastest")
+tree(5, 100, angle=40, scale=0.4)
+t.hideturtle()
+
+teleport(200, -150)
+t.width(1)
+t.speed("fastest")
+tree(9, 50, angle=35, scale=0.95, rand_pc=30)
+t.hideturtle()
+
+save_to("tree_3")
